0ccf189345470cd11b0d9fe665f887ad49c28d45
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2020-09-16 16:48:01 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,17 +1,15 @@
 import datetime as dt
-# columns (переменные)
-
-limit = 2500
-
+import pytest as pt
 
 class Calculator:
-    def __init__(self, limit):
-        self.limit = limit
+    limit = 2500
+    def __init__(self,limit):
         self.records = []
-    # Add in operation list(добавление в лист операций)
+        self.limit = limit
+
     def add_record(self, record):
         self.records.append(record)
-    # Spent in a day (потрачено за день)
+
     def get_today_stats(self, records):
         now_time = dt.datetime.now()
         today_status = 0
@@ -20,68 +18,71 @@ class Calculator:
             if records.date == now_time:
                 today_status += record.amount
         return today_status
-    # Spent in a week (считает затраты за неделю)
+
     def get_week_stats(self):
         week_stats = 0
-        today = dt.datetime.today().date()
+        today = dt.datetime.today()
         delta_date = today - dt.timedelta(days=7)
-
         for record in self.records:
-            if week_ago < record.date <= today:
-
+            if delta_date <= record.date <= today:
                 week_stats += record.amount
         return delta_date
 
-   # Support class(вспомогательный класс)
+
+
+
 class Record:
-    def __init__(self, amount, date, comment):
-        self.amount = amount
-        self.date = now_time
-        self.comment = comment
+   def __init__(self, amount, comment,date = (dt.datetime.today()).date()):
+       self.amount = summ
+       self.comment = comment
+       if type(date) is str:
+           self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
+       else:
+           self.date = date
 
-        if type(date) is str:
-            moment = dt.datetime.strptime(date, '%d.%m.%Y')
-            self.date = moment.date()
+
+class CashCalculator(Calculator):
+    EURO_RATE = 80
+    USD_RATE = 75
+    RUB_RATE = 1
+    CURRENCIES = {
+        "rub": ["руб", 1],
+        "usd": ["USD", USD_RATE],
+        "eur": ["Euro", EURO_RATE],
+    }
+
+    def get_today_cash_remained(self,currency):
+
+
+        remained = self.limit - self.get_today_status()
+        if remained < self.limit:
+            return f'На сегодня осталось {remained} {currency}'
+        elif remainednder == 0:
+            return f'Денег нет, держись'
         else:
-            self.date = date
+            return f'Денег нет, держись: твой долг - {remained} {currency}'
 
-# Counts the number of calories spent
-# (считает потраченные калории)
+    def get_week_cash_remained(self,currency):
+        weekly_remain = self.get_week_stats()
+        return f'За последнюю неделю было потрачено {weekly_remain} {currency}'
 class CaloriesCalculator(Calculator):
 
     def get_calories_remained(self):
-        today_calor_spent = super().get_today_stats()
-        calor = self.limit - today_calor_spent
-        if self.limit > today_nolim:
-            print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calor} кКал')
+        balance = self.limit - self.get_today_stats()
+        if balance > 0:
+            print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал')
         else:
-            print('Хватит есть!')
-    def week_calories(self,get_week_stats,week_result):
-        self.week_result = get_week_stats
-        return f'За последнюю неделю всего получено {week_result} кКал'
-# Calculate money spends
-class CashCalculator(Calculator):
-    def get_today_cash_remained(self,cours,limit,today_stats,cash,difference):
-        EURO_RATE = 89
-        USD_RATE = 75
-        RUB_RATE = 1
-        difference = limit - today_stats
-        cours = {'USD':{'name':'USD','rate':self.USD_RATE},
-                 'EUR':{'name':'EUR','rate':self.EURO_RATE},
-                 'RUB':{'name':'RUB','rate':self.RUB_RATE}}
+            print(f'Хватит есть!')
 
 
-        if difference < limit:
-            remainder = float
-            return f'На сегодня осталось {difference} {cours}'
 
-        elif difference >= limit:
-            cash = float(abs(cash))
-            return f'Денег нет, держись: твой долг {cash} {currency}'
-        else:
-            return f'Денег нет, держись'
-# Week spends money
-    def cash_spend_week(self, get_week_stats, cours):
-        cash_spend = float(get_week_stats())
 
-        return f'На неделе ты потратил {cash_spend} {cours}'
+
+
+
+
+
+
+
+
+       
