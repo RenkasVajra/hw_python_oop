9bed742310f5dade4d0a4ed4fb7398347bcdad06
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2020-09-16 19:05:20 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -5,8 +5,10 @@ class Calculator:
     def __init__(self, limit):
         self.limit = limit
         self.records = []
+
     def add_record(self, record):
         self.records.append(record)
+
     def get_today_stats(self):
         status = 0
         today = (dt.datetime.today()).date()
@@ -14,14 +16,14 @@ class Calculator:
             if record.date == (dt.datetime.today()).date():
                 status += record.amount
         return status
+
     def get_week_stats(self):
         status = 0
         today = (dt.datetime.today()).date()
-        week_ago = today_date - dt.timedelta(days=6)
+        week_ago = today - dt.timedelta(days=6)
         for record in self.records:
             if week_ago <= record.date <= today:
                 status += record.amount
-
         return status
 
 
@@ -31,6 +33,7 @@ class Record:
    def __init__(self, amount, comment,date = (dt.datetime.today()).date()):
        self.amount = amount
        self.comment = comment
+
        if type(date) is str:
            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
        else:
@@ -38,8 +41,8 @@ class Record:
 
 
 class CashCalculator(Calculator):
-    EURO_RATE = 88
-    USD_RATE = 75
+    EURO_RATE = 70
+    USD_RATE = 60
     CURRENCIES = {
         "usd":{"RATE":USD_RATE,'name': 'USD'},
         "rub":{"RATE": 1, 'name': "руб"},
@@ -48,6 +51,7 @@ class CashCalculator(Calculator):
 
     def get_today_cash_remained(self,currency):
         remained = self.limit - self.get_today_stats()
+
         if remained > 0:
             return f'На сегодня осталось {remained} {currency}'
         elif remained == 0:
@@ -57,13 +61,13 @@ class CashCalculator(Calculator):
 
 
 class CaloriesCalculator(Calculator):
-
     def get_calories_remained(self):
         balance = self.limit - self.get_today_stats()
+
         if balance > 0:
-            print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал')
+            print (f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал')
         else:
-            print(f'Хватит есть!')
+            print (f'Хватит есть!')
 
 
 
