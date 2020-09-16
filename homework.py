d7b47f187cdd6b751198bf36e99dedff6cce85ff
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2020-09-16 17:54:18 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -9,18 +9,19 @@ class Calculator:
         self.records.append(record)
     def get_today_stats(self):
         status = 0
-        today = (dt.datetime.today()).date()
+        today_date = (dt.datetime.today()).date()
         for record in self.records:
             if record.date == (dt.datetime.today()).date():
                 status += record.amount
         return status
     def get_week_stats(self):
         status = 0
-        delta_date = (dt.datetime.today()).date()
-        week_ago = today - dt.timedelta(days=6)
+        today_date = (dt.datetime.today()).date()
+        week_ago = today_date - dt.timedelta(days=6)
         for record in self.records:
             if week_ago <= record.date <= today:
                 status += record.amount
+
         return status
 
 
@@ -42,12 +43,11 @@ class CashCalculator(Calculator):
     CURRENCIES = {
         "usd":{"RATE":USD_RATE,'name': 'USD'},
         "rub":{"RATE": 1, 'name': "руб"},
-        "eur":{'RATE': EURO_RATE, 'name':'Eiro'}
+        "eur":{'RATE': EURO_RATE, 'name':'Euro'}
         }
 
     def get_today_cash_remained(self,currency):
         remained = self.limit - self.get_today_stats()
-        amount - self.CURRENCIES[currency]['name']
         if remained > 0:
             return f'На сегодня осталось {remained} {amount}'
         elif remained == 0:
@@ -55,7 +55,7 @@ class CashCalculator(Calculator):
         else:
             return f'Денег нет, держись: твой долг - {remained} {amount}'
 
-    
+
 class CaloriesCalculator(Calculator):
 
     def get_calories_remained(self):
@@ -64,4 +64,3 @@ class CaloriesCalculator(Calculator):
             print(f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {balance} кКал')
         else:
             print(f'Хватит есть!')
-
