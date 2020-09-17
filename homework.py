336435472b782ddd68840cf1a0576b4c5deced36
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2020-09-17 19:32:49 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -62,7 +62,7 @@ class CashCalculator(Calculator):
             return f'На сегодня осталось {remained} {currency_name}'
         if remained == 0:
             return f'Денег нет, держись'
-        return f'Денег нет, держись: твой долг - {remained} {abs(currency_name)} '
+        return f'Денег нет, держись: твой долг - {remained} {currency_name} '
 
 
 class CaloriesCalculator(Calculator):
@@ -74,7 +74,6 @@ class CaloriesCalculator(Calculator):
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories} кКал'
         else:
             return f'Хватит есть!'
-
 
 
 
