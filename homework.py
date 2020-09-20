7be5c7afcd92a819e22c1e29b62b3529de6ced34
Андрей <71021948+RenkasVajra@users.noreply.github.com>
2020-09-20 09:41:25 +0500

    Update homework.py

--- homework.py
+++ homework.py
@@ -1,5 +1,5 @@
 import datetime as dt
-import pytest as pt
+
 
 class Calculator:
     def __init__(self, limit):
@@ -11,9 +11,9 @@ class Calculator:
 
     def get_today_stats(self):
         status = 0
-        today = (dt.datetime.today()).date()
+        today = dt.date.today()
         for record in self.records:
-            if record.date == (dt.datetime.today()).date():
+            if record.date == today:
                 status += record.amount
         return status
 
@@ -27,17 +27,15 @@ class Calculator:
         return status
 
 
-
-
 class Record:
-   def __init__(self, amount, comment,date = (dt.datetime.today()).date()):
+   def __init__(self, amount, comment, date = None ):
        self.amount = amount
        self.comment = comment
 
-       if type(date) is str:
+       if date is not None:
            self.date = dt.datetime.strptime(date, '%d.%m.%Y').date()
        else:
-           self.date = date
+           self.date = dt.date.today()
 
 
 class CashCalculator(Calculator):
@@ -50,19 +48,17 @@ class CashCalculator(Calculator):
         "eur": {'RATE': EURO_RATE, 'name': 'Euro'}
     }
 
-
-    def get_today_cash_remained(self,currency):
-
+    def get_today_cash_remained(self, currency):
 
         balance = self.limit - self.get_today_stats()
-        remained = round(balance / self.CURRENCIES[currency]["RATE"], 2)
+        remained = abs(round(balance / self.CURRENCIES[currency]["RATE"], 2))
         amount = self.CURRENCIES[currency]["name"]
 
         if balance > 0:
-            return f'На сегодня осталось {abs(remained)} {amount}'
+            return f'На сегодня осталось {remained} {amount}'
         if balance == 0:
             return f'Денег нет, держись'
-        return f'Денег нет, держись: твой долг - {abs(remained)} {amount}'
+        return f'Денег нет, держись: твой долг - {remained} {amount} '
 
 
 class CaloriesCalculator(Calculator):
@@ -72,10 +68,7 @@ class CaloriesCalculator(Calculator):
 
         if calories > 0:
             return f'Сегодня можно съесть что-нибудь ещё, но с общей калорийностью не более {calories} кКал'
-        else:
-            return f'Хватит есть!'
-
-
+        return f'Хватит есть!'
 
 
 
